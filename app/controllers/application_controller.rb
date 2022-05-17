26c41a22a45f544a49eed1d085e74862a83b9843
Patricia Mulaudzi <32956237+Patricious@users.noreply.github.com>
2022-05-17 10:19:47 +0200

    created controllers and views for sorting the sequence

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    # to prevent Cross-Site Request Forgery (CSRF) attacks by raising exception.
+    # If you use :null_session for APIs.
+    protect_from_forgery with: :exception
 end
---
+++ app/interactors/determine_sequence.rb
@@ -0,0 +1,7 @@
+class DetermineSequence
+    include Interactor::Organizer
+  
+    organize ParseJobs, SortJobs
+  end
+
+  # this is for sorting and organizing.
